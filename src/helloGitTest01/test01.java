7133db53708574a1918ab3ed92e51d508fbe1eeb
chosm <chosm@DESKTOP-GI68G0I>
2022-03-10 23:39:00 +0900

    Git 연결 TEST

---
+++ src/helloGitTest01/test01.java
@@ -0,0 +1,11 @@
+package helloGitTest01;
+
+public class test01 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("helloGitTest01..^---^!!");
+
+	}
+
+}
