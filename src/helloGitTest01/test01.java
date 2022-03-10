192f5b50e657c50c06ffc3a63239f5a8926cbdc3
onespace2022 <101251035+onespace2022@users.noreply.github.com>
2022-03-11 00:30:59 +0900

    Update test01.java

--- src/helloGitTest01/test01.java
+++ src/helloGitTest01/test01.java
@@ -6,6 +6,7 @@ public class test01 {
 		// TODO Auto-generated method stub
 		System.out.println("helloGitTest01..^---^!!");
 		System.out.println("helloGitTest02..^---^!!");
+		System.out.println("helloGitTest03..^---^!!");
 	}
 
 }
