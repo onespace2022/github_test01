d742f9c277219db2a4cfdc6e52be2a3b10ac6e1e
chosm <chosm@DESKTOP-GI68G0I>
2022-03-11 00:15:41 +0900

    수정사항 있음

--- src/helloGitTest01/test01.java
+++ src/helloGitTest01/test01.java
@@ -5,7 +5,7 @@ public class test01 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("helloGitTest01..^---^!!");
-
+		System.out.println("helloGitTest02..^---^!!");
 	}
 
 }
